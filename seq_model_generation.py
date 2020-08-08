16078d8c1a52edd3289be47f9227d5dcab6f4871
renko7 <jotakaemail@gmail.com>
2020-08-08 01:36:10 -0400

    Data Generation for Model, Creation and training of Sequential Model
    The data_generation.py generates data which is used for the trial. seq_model_generation generates a sequential model with one hidden layer. model_train formats and trains the Sequential Model.

---
+++ seq_model_generation.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+
+#units are arbitralty set
+#first dense is hidden layer, input_shape because input is one dimensional
+
+model = Sequential([
+    Dense(units=16, input_shape=(1,), activation='relu'),
+    Dense(units=32, activation='relu'),
+    Dense(units=2, activation='softmax')
+])
+
+model.summary()
+
